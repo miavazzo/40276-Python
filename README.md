d5e82dea0c9d093972c60aa2cb103f3f4fd8f389
Massimiliano Iavazzo <massimiliano.iavazzo@capgemini.com>
2024-07-31 11:41:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Email API Project
+
+Questa è una semplice API Flask per inviare email con o senza allegati utilizzando Microsoft Graph API.
+
+## Setup
+
+1. Crea un ambiente virtuale:
+   ```sh
+   python -m venv venv
---
+++ app/routes.py
@@ -0,0 +1,25 @@
+from flask import request, jsonify, current_app
+from .email_service.py import send_email_with_attachment
+
+@current_app.route('/send_email', methods=['POST'])
+def send_email():
+    api_key = request.headers.get('x-api-key')
+    if api_key != current_app.config['API_KEY']:
+        return jsonify({"error": "Unauthorized"}), 401
+
+    data = request.json
+    subject = data.get('subject')
+    body = data.get('body')
+    to_email = data.get('to_email')
+    attachment = data.get('attachment', None)
+
+    if attachment:
+        attachment_path = "attachment"
+        with open(attachment_path, "wb") as f:
+            f.write(base64.b64decode(attachment))
+    else:
+        attachment_path = None
+
+    result = send_email_with_attachment(subject, body, to_email, attachment_path)
+
+    return jsonify(result), 200
