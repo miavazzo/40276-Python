11c1afb4aaff11d0069196da9ab54cb82af87e57
Massimiliano Iavazzo <massimiliano.iavazzo@capgemini.com>
2024-07-31 20:55:43 +0200

    v2.0.1 modifica test_email_service.py

--- tests/test_email_service.py
+++ tests/test_email_service.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import patch, MagicMock
+import os
 import app.email_service as email_service
 
 class TestEmailService(unittest.TestCase):
@@ -9,10 +10,16 @@ class TestEmailService(unittest.TestCase):
         mock_post.return_value.status_code = 202
         mock_post.return_value.json.return_value = {}
 
-        subject = "Test Email"
+        subject = "40276-Test Email"
         body = "This is a test email."
-        to_email = "test@example.com"
-        attachments = [{"path": "testfile.txt", "filename": "testfile.txt"}]
+        to_email = "massimiliano.iavazzo@capgemini.com"
+        attachments = [
+            {"path": r"C:\Users\miavazzo\OneDrive - Capgemini\Documents\T. 40276 parametri email portale clienti - fatture Newatt\Crypto101.pdf", "filename": "Crypto101.pdf"},
+            {"path": r"C:\Users\miavazzo\OneDrive - Capgemini\Documents\T. 40276 parametri email portale clienti - fatture Newatt\Form-Argon-Design-Form.pdf", "filename": "Form-Argon-Design-Form.pdf"},
+            {"path": r"C:\Users\miavazzo\OneDrive - Capgemini\Documents\T. 40276 parametri email portale clienti - fatture Newatt\test1.docx", "filename": "test1.docx"},
+            {"path": r"C:\Users\miavazzo\OneDrive - Capgemini\Documents\T. 40276 parametri email portale clienti - fatture Newatt\test2.docx", "filename": "test2.docx"},
+            {"path": r"C:\Users\miavazzo\OneDrive - Capgemini\Documents\T. 40276 parametri email portale clienti - fatture Newatt\test3.docx", "filename": "test3.docx"}
+        ]
 
         result = email_service.send_email_with_attachments(subject, body, to_email, attachments)
         self.assertEqual(result["status"], "Email inviata con successo!")
@@ -22,10 +29,16 @@ class TestEmailService(unittest.TestCase):
         mock_post.return_value.status_code = 400
         mock_post.return_value.json.return_value = {"error": "BadRequest"}
 
-        subject = "Test Email"
+        subject = "40276-Test Email"
         body = "This is a test email."
-        to_email = "test@example.com"
-        attachments = [{"path": "testfile.txt", "filename": "testfile.txt"}]
+        to_email = "massimiliano.iavazzo@capgemini.com"
+        attachments = [
+            {"path": r"C:\Users\miavazzo\OneDrive - Capgemini\Documents\T. 40276 parametri email portale clienti - fatture Newatt\Crypto101.pdf", "filename": "Crypto101.pdf"},
+            {"path": r"C:\Users\miavazzo\OneDrive - Capgemini\Documents\T. 40276 parametri email portale clienti - fatture Newatt\Form-Argon-Design-Form.pdf", "filename": "Form-Argon-Design-Form.pdf"},
+            {"path": r"C:\Users\miavazzo\OneDrive - Capgemini\Documents\T. 40276 parametri email portale clienti - fatture Newatt\test1.docx", "filename": "test1.docx"},
+            {"path": r"C:\Users\miavazzo\OneDrive - Capgemini\Documents\T. 40276 parametri email portale clienti - fatture Newatt\test2.docx", "filename": "test2.docx"},
+            {"path": r"C:\Users\miavazzo\OneDrive - Capgemini\Documents\T. 40276 parametri email portale clienti - fatture Newatt\test3.docx", "filename": "test3.docx"}
+        ]
 
         result = email_service.send_email_with_attachments(subject, body, to_email, attachments)
         self.assertIn("Errore nell'invio dell'email.", result["status"])
