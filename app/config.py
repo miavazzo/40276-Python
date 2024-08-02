eb04aa828d89c88581d6cadfc59e3b48411df104
Massimiliano Iavazzo <massimiliano.iavazzo@capgemini.com>
2024-08-02 02:22:12 +0200

    v1.1-Modify var name on .env

--- app/config.py
+++ app/config.py
@@ -1,12 +1,18 @@
+'''
+dependendies import section
+'''
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Config:
+    '''
+    configuration file for the app
+    '''
     SECRET_KEY = os.getenv('SECRET_KEY') #os.environ.get('SECRET_KEY', 'default_secret_key')
-    CLIENT_ID = os.getenv('CLIENT_ID') #os.environ.get('CLIENT_ID')
+    CLIENT_ID = os.getenv('CLIENT_IDS') #os.environ.get('CLIENT_ID')
     CLIENT_SECRET = os.getenv('CLIENT_SECRET') #os.environ.get('CLIENT_SECRET')
-    TENANT_ID = os.getenv('TENANT_ID') #os.environ.get('TENANT_ID')
+    TENANT_ID = os.getenv('TENANT_IDS') #os.environ.get('TENANT_ID')
     USERNAME = os.getenv('APP_USERNAME') #os.environ.get('USERNAME')
     API_KEY = os.getenv('APP_API_KEY') #os.environ.get('API_KEY')
