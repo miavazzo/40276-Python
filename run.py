ea913a4b93983d41d192ea4e2e475a4c0f801034
Massimiliano Iavazzo <massimiliano.iavazzo@capgemini.com>
2024-08-07 19:24:30 +0200

    v3.4-Aggiunta fork per produzione e sviluppo

--- run.py
+++ run.py
@@ -2,10 +2,17 @@
 file per startare l'applicazione
 '''
 import os
+from dotenv import load_dotenv
 from app import create_app
+
+# Carica le variabili d'ambiente dal file .env
+load_dotenv()
 
 config_name = os.getenv('FLASK_ENV') or 'development'
 app = create_app(config_name)
 
 if __name__ == "__main__":
+    '''
+    l'applicazione viene avviata in modalità sviluppo o produzione
+    '''
     app.run(host='0.0.0.0', port=5000)
