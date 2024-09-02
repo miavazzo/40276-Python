3f58ec1873e13ffc3b39aecb0451998118fbcf9d
Massimiliano Iavazzo <massimiliano.iavazzo@capgemini.com>
2024-09-02 15:49:39 +0200

    Aggiunta service.py per generazione del servizio Windows da installare sulla macchina del cliente

--- service.py
+++ service.py
@@ -3,6 +3,7 @@ import win32service
 import win32event
 import servicemanager
 import socket
+import logging
 from app import create_app  # Assicurati di importare correttamente la tua applicazione Flask da run.py
 
 class AppServerSvc(win32serviceutil.ServiceFramework):
@@ -14,10 +15,16 @@ class AppServerSvc(win32serviceutil.ServiceFramework):
         win32serviceutil.ServiceFramework.__init__(self, args)
         self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
         socket.setdefaulttimeout(60)
+        logging.basicConfig(
+            filename='service.log',
+            level=logging.DEBUG,
+            format='%(asctime)s - %(levelname)s - %(message)s'
+        )
 
     def SvcStop(self):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
         win32event.SetEvent(self.hWaitStop)
+        logging.info("Service is stopping.")
 
     def SvcDoRun(self):
         servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
@@ -26,8 +33,13 @@ class AppServerSvc(win32serviceutil.ServiceFramework):
         self.main()
 
     def main(self):
-        app = create_app()  # Crea l'applicazione Flask
-        app.run()  # Avvia l'applicazione Flask
+        logging.info("Service is starting.")
+        try:
+            app = create_app()  # Crea l'applicazione Flask
+            app.run()  # Avvia l'applicazione Flask
+        except Exception as e:
+            logging.error(f"Error in main: {str(e)}")
+            raise
 
 if __name__ == '__main__':
     win32serviceutil.HandleCommandLine(AppServerSvc)
